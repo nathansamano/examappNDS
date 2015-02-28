31706e7f89dfea20dc3edef45e153e92f4e39ecc
Nathan Samano <nsv9982@saintjoe.edu>
2015-02-27 21:04:11 -0500

    show how to parse data

--- fillin/takefillin.js
+++ fillin/takefillin.js
@@ -1,9 +1,15 @@
 // load the fillin data
 var data = require('./fillin.js');
-//var fillin = JSON.parse(data);
 
-(function() {
-  
-  console.log(data);
+console.log(typeof data);
+console.log(data.length);
+var oneRec = data[0];
+data.forEach(function(entry) {
+  console.log(entry.text);
+  if (entry.values.length > 1) 
+	// punt for now
+     console.log('Found multi-element array');
+   else
+     console.log(entry.values);
+   });
 
-})();
